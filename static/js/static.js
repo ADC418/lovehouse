7c4d99749714b3e0d76d67a6d3a9e8a9516034d4
liuzheng <15515082313>
2020-08-22 17:19:36 +0800

    对小屋快讯页面进行完善

---
+++ static/js/static.js
@@ -0,0 +1,8 @@
+// $('.xwkx_box').hover(function(){
+//     $('.xukx_hover').show()
+
+//     console.log($('.xukx_hover'))
+// },function(){
+//     $('.xwkx_more').hide()
+//     console.log(2)
+// })
