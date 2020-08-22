faf0fd8ccaca61d3ad07b8ed21ecbcb744cc2570
mazhenzhen <2393088061@qq.com>
2020-08-22 13:07:00 +0800

    完善关于企业的介绍部分

---
+++ static/js/gyqy.js
@@ -0,0 +1,21 @@
+$(function(){
+    //首页、关于企业鼠标经过变红
+    // 先写要改变的值，再写原来的颜色
+
+    // 第一种方法，使用鼠标移入移出的方式
+    // $('.gyqy_a').mouseenter(function(){
+    //     $(this).css('color','#ff5722')
+    // })
+    // $('.gyqy_a').mouseleave(function(){
+    //     $(this).css('color','#b0b0b0')
+    // })
+
+    //使用hover的方式
+    $('.gyqy_a').hover(function(){
+        $(this).css('color','#ff5722')
+    },function(){
+        $(this).css('color','#b0b0b0')
+    })
+ 
+ 
+})
