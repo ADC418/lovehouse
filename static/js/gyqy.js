5b7c164b410fe785f6b55715b04ce03865983187
mazhenzhen <2393088061@qq.com>
2020-08-24 15:11:39 +0800

    完善拖动轮播和六大竞争模块

--- static/js/gyqy.js
+++ static/js/gyqy.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     //首页、关于企业鼠标经过变红
     // 先写要改变的值，再写原来的颜色
 
@@ -11,11 +11,26 @@ $(function(){
     // })
 
     //使用hover的方式
-    $('.gyqy_a').hover(function(){
-        $(this).css('color','#ff5722')
-    },function(){
-        $(this).css('color','#b0b0b0')
+    $('.gyqy_a').hover(function () {
+        $(this).css('color', '#ff5722')
+    }, function () {
+        $(this).css('color', '#b0b0b0')
     })
- 
- 
+
+
+
+    // 使用swiper制作拖动轮播图
+    var mySwiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        // pagination: {
+        //     el: '.swiper-pagination',
+        //     clickable: true,
+        // },
+
+    })
+
+
 })
